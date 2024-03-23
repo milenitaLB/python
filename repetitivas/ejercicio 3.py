30258eac50b5e62165bd8d376853ae1eff8e2684
Brahian Parra <brahianparra933@gmail.com>
2024-03-23 14:57:51 -0500

    ejercicios

---
+++ repetitivas/ejercicio 3.py
@@ -0,0 +1,21 @@
+opcion = 0
+print("Bienvenido al menú:")
+print("1. sumar")
+print("2. restar")
+print("3. multiplicar")
+print("4. dividir")
+print("5. salir")
+opcion=int(input("Digite la opción: "))
+if opcion == 1:
+        print("sumar")
+
+elif opcion == 2:
+        print("restar")
+elif opcion == 3:
+        print("multiplicar.")
+elif opcion == 4:
+        print("dividir")
+elif opcion == 5:
+        print("Saliendo del menú.")
+else:
+        print("Opción inválida. Por favor, seleccione una opción válida.")
